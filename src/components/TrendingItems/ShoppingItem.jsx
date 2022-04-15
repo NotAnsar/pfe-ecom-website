bf6e6fdd742abd947934fd493ac5a7c4bcbcf7bc
NotAnsar <karrouach.ansar@gmail.com>
2022-04-15 01:38:22 +0000

    images issue fixed probably3

--- src/components/TrendingItems/ShoppingItem.jsx
+++ src/components/TrendingItems/ShoppingItem.jsx
@@ -4,8 +4,6 @@ import { Link } from 'react-router-dom';
 import classes from './TrendingItems.module.scss';
 
 const ShoppingItem = ({ image, name, price, id }) => {
-  console.log(`./images/${image}`);
-
   return (
     <Link to={`/product/${id}`} className={classes.itm}>
       <div className={classes.rowgrid}>
