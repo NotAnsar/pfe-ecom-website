652be96021fa5a535e18f6c5adf63e879b8b585c
NotAnsar <karrouach.ansar@gmail.com>
2022-05-26 11:46:21 +0100

    sort redesigned

--- src/components/ShopPage/ShopHeader.jsx
+++ src/components/ShopPage/ShopHeader.jsx
@@ -7,7 +7,7 @@ const ShopHeader = () => {
 			<h1>Shop All</h1>
 			<div className={classes.sortBy}>
 				<div className={classes.sortBy}>
-					<label htmlFor='sort'>Sort By</label>
+					{/* <label htmlFor='sort'>Sort By</label> */}
 					<select name='sort' defaultValue={'default'} className={classes.sort}>
 						<option value='default'>Default</option>
 						<option value='AtoZ'>Name A-Z</option>
@@ -16,7 +16,7 @@ const ShopHeader = () => {
 						<option value='decroi'>Price (High to Low)</option>
 					</select>
 				</div>
-				<div className={classes.sortBy}>
+				{/* <div className={classes.sortBy}>
 					<label htmlFor='show'>Show</label>
 					<select name='show' className={classes.show} defaultValue={'9'}>
 						<option value='6'>6</option>
@@ -25,7 +25,7 @@ const ShopHeader = () => {
 						<option value='15'>15</option>
 						<option value='18'>18</option>
 					</select>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
